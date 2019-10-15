0012200256df1a0810e096b3d0bfce2477a8fd9d
Nandeeka Nayak <nandeeka@gmail.com>
2019-10-15 14:52:52 -0700

    Add script to add environment variables before running factor

---
+++ scripts/run_factor.sh
@@ -0,0 +1,3 @@
+export LD_LIBRARY_PATH="/Users/nandeekanayak/Documents/College/Senior Year/Fall 2019/CS 183/numerical-scratch/lib"
+export DYLD_LIBRARY_PATH="/Users/nandeekanayak/Documents/College/Senior Year/Fall 2019/CS 183/numerical-scratch/lib"
+~/sources/factor/factor
